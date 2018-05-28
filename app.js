1e13a96b0f5dc800fd211af03ab7836b2483f5dc
ConstanceEDJ <constance.daire@sciencespo.fr>
2018-05-28 11:49:17 +0200

    structure

--- app.js
+++ app.js
@@ -3,20 +3,40 @@ $(document).ready(function() {
   var controller = new ScrollMagic.Controller();
 
   var wipeAnimation = new TimelineMax()
-    .fromTo('section#page2', 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone }) // From botton
-    .fromTo('section#page3', 1, { x: '100%' }, { x: '0%', ease: Linear.easeNone }) // From right
-    })
+    .fromTo('section#chapitre-1', 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone }) // From bottom
+    .fromTo('section#chapitre-2', 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone }) // From right
+    .fromTo('section#chapitre-3', 1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })// From right
+    .fromTo('section#chapitre-4', 1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })// From right
+    .fromTo('section#chapitre-5', 1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })// From right
 
 
-  // create a scene
-  new ScrollMagic.Scene({
-      triggerElement: '#intro',
+// create a scene
+ new ScrollMagic.Scene({
+      triggerElement: '#container',
       triggerHook: 'onLeave',
-      duration: '200%' // = nombre de slides * 100 (pour un scroll naturel)
+      duration: '600%' // = nombre de slides * 100 (pour un scroll naturel)
     })
-    .setPin('#intro')
+    .setPin('#container')
     .setTween(wipeAnimation)
-    
+    .on("update", function (scene) {
+      const progress = scene.target.progress()
+       if (progress > 0.50 && progress < 0.66) {
+        startVideo('chapitre-3')
+      } else {
+        stopVideo('chapitre-3')
+      }
+    })
     .addIndicators()
     .addTo(controller);
 });
+
+
+function startVideo(chapitre) {
+  $('#'+chapitre+' video').addClass('active')
+  $('#'+chapitre+' video')[0].play()
+}
+
+function stopVideo(chapitre) {
+  $('#'+chapitre+' video').removeClass('active')
+  $('#'+chapitre+' video')[0].pause()
+}
